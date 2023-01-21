769767f7bc9994fec65b8afd43553d334ab23218
Antonny Mello <antonny.mello@gmail.com>
2023-01-21 15:08:51 -0300

    refactor: simplify missing parameter code

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,12 +4,17 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 
 class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }
