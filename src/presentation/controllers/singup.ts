43cec955ef95286d6cbc173d8b498a79d6ae7f34
Antonny Mello <antonny.mello@gmail.com>
2023-01-21 15:14:02 -0300

    feat: ensure SignUpController returns 400 and a error if passwordConfirmation was not received

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,7 +4,12 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 
 class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password'];
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
