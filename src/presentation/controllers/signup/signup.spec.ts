072450c35bd9b69d8b6a7d899df42c6746ccc775
Antonny Mello <antonny.mello@gmail.com>
2023-01-22 20:02:42 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -247,4 +247,29 @@ describe('SignUp Controller', () => {
       password: 'any_password',
     });
   });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut, addAccountStub } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      },
+    };
+
+    addAccountStub.add(httpRequest.body);
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    });
+  });
 });
