0411a7e827677957bfeb7db8426bcae728648bcc
Antonny Mello <antonny.mello@gmail.com>
2023-02-03 09:21:52 -0300

    test: ensure DbAddAccount throws error when addAccountRepository throws errors

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -111,4 +111,24 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_password',
     });
   });
+
+  test('Should throw if AddAccountRepository throws error', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error());
+      })
+    );
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    };
+
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
