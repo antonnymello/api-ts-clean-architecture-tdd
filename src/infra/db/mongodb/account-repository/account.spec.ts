3f3a986400f79d84911a271a09cbf4b8bbe69811
Antonny Mello <antonny.mello@gmail.com>
2023-02-28 14:31:19 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository ', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return an new account on success', async () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_mail@mail.com',
+      password: 'any_hashed_password',
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_mail@mail.com');
+    expect(account.password).toBe('any_hashed_password');
+  });
+});
